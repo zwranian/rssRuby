d60b2784d257f855f3fbffed1ec8d87614210780
Roy_n_roy <euphsuzuki@yahoo.co.jp>
2013-04-20 02:33:32 +0900

    まだ途中()

--- pocket/getCode.rb
+++ pocket/getCode.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 require 'uri'
-require 'oauth'
+#require 'oauth'
 require 'net/https'
 require 'net/http'
 require 'pp'
@@ -13,16 +13,15 @@ include AccessToken_Pocket
 uri = URI.parse("https://getpocket.com/v3/oauth/request")
 https = Net::HTTP.new(uri.host,uri.port)
 https.use_ssl = true
-https.ca_file = "./cacert.pem"
+https.ca_file = OpenSSL::X509::DEFAULT_CERT_FILE
+p OpenSSL::X509::DEFAULT_CERT_FILE
 https.verify_depth = 5
 https.verify_mode = OpenSSL::SSL::VERIFY_PEER
 https.start do |hs|
     request = Net::HTTP::Post.new(uri.request_uri)
     request["Content-Type"] = "application/json; charset=UTF-8"
     request["X-Accept"] = "application/json"
-    request.body = {
-        consumer_key: @consumer_key,
-        redirect_uri:"http://www.google.com"}.to_json
+    request.body = {'consumer_key'=> @consumer_key, 'redirect_uri'=> "http://localhost"}.to_json
     hs.request(request) do |response|
         status = JSON.parse(response.body)
         code = status['code']

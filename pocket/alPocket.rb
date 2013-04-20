b2d2d88113be03cc1bf7a9d5e34eccb801a62424
Roy_n_roy <a>
2013-04-21 02:20:12 +0900

    正常動作
    未:メソッドprivate化
    未:post時自動ログイン

--- pocket/alPocket.rb
+++ pocket/alPocket.rb
@@ -11,6 +11,7 @@ class ALPocket
 
     def initialize
         @consumer_key = CONSUMERKEY_POCKET
+        @access_token = nil
         @redirect_uri = REDIRECT_URI
     end
 
@@ -38,12 +39,16 @@ class ALPocket
                     status = JSON.parse(response.body)
                     request_token =  status['code']
                 rescue JSON::ParserError
-                    p "error getRequestToken:"+response.body.to_s
                 end
             end
         end
-        if response=Net::HTTPOK
+        
+        #取得エラーチェック
+        case response
+        when Net::HTTPOK
             return request_token
+        else
+            p "error getRequestToken:"+response.body.to_s
         end
     end
 
@@ -51,6 +56,8 @@ class ALPocket
         #インスタンス生成、IE偽装
         agent = Mechanize.new
         agent.user_agent_alias = 'Windows IE 9'
+        agent.redirect_ok = 'true'
+        agent.redirection_limit = 1
 
         #ログインページへアクセス
         uri = URI.parse("https://getpocket.com/auth/authorize?request_token="+code+"&redirect_uri"+@redirect_uri)
@@ -64,7 +71,15 @@ class ALPocket
         form.field_with(:name => 'password').value = passwd
 
         #formをsubmit
-        form.click_button
+        begin
+            page = form.click_button
+        rescue Mechanize::RedirectLimitReachedError
+        end
+        
+        #ログインエラーチェック
+        if page.uri != @redirect_uri
+            p "erro login: "
+        end
     end
 
     def getAccessToken(code)
@@ -87,26 +102,63 @@ class ALPocket
                     status = JSON.parse(response.body)
                     access_token = status['access_token']
                 rescue JSON::ParserError
-                    p "error getAccessToken:"+response.body.to_s
                 end
             end
         end
-        if response=Net::HTTPOK
+        
+        #取得エラーチェック
+        case response
+        when Net::HTTPOK
+            @access_token = access_token
             return access_token
+        else
+            p "error getAccessToken:"+response.body.to_s
+        end
+    end
+    
+    def post(url, title, tweetid)
+        uri = URI.parse("https://getpocket.com/v3/add")
+        https = Net::HTTP.new(uri.host,uri.port)
+        https.use_ssl = true
+        https.ca_file = OpenSSL::X509::DEFAULT_CERT_FILE
+        https.verify_depth = 5
+        https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        response = https.start do |hs|
+            request = Net::HTTP::Post.new(uri.request_uri)
+            request["Content-Type"] = "application/json; charset=UTF-8"
+            request["X-Accept"] = "application/json"
+            request.body = {'url'=> url, 'title'=> title, 'tweet_id'=> tweetid,
+               'consumer_key'=> @consumer_key,
+               'access_token'=> @access_token
+                }.to_json
+            hs.request(request)
+        end
+        
+        #投稿エラーチェック
+        case response
+        when Net::HTTPClientError
+            p "error post:"+response.body.to_s
         end
     end
 end
 
 
+# テストコード #
 pocket = ALPocket.new
+
+#テストポスト(エラー)
+pocket.post("http://google.com","テストグーグル",nil)
 
 #リクエストトークン取得&表示
 req_tkn = pocket.getRequestToken
-p req_tkn
+p "main requestToken: "+req_tkn.to_s
 
 #Webログイン(PocketID,Passwordを設定)
 pocket.login("ID","PASS",req_tkn)
 
 #アクセストークン取得&表示
 acs_tkn = pocket.getAccessToken(req_tkn)
-p acs_tkn
+p "main accessToken: "+acs_tkn.to_s
+
+#テストポスト
+pocket.post("http://google.com","テストグーグル",nil)

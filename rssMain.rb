77242d18fbb2d0775d7043be267c83fba2693d01
zwranian <zawa.pc@gmail.com>
2013-05-17 00:56:10 +0900

    revert前に戻す

--- rssMain.rb
+++ rssMain.rb
@@ -1,13 +1,12 @@
 #!/usr/bin/ruby
 require '/home/pi/rss/rssAnalyzer'
 require 'rexml/document'
-require 'tempfile'
 require 'set'
 include REXML
 #初期処理
-tmp = Tempfile::new("tmp","/home/pi/rss/")
 rssAry = Array.new
 hisSet = Set.new
+postAry = Array.new
 sites = open("/home/pi/rss/rssResource")
 history = open("/home/pi/rss/rssHistory","r+")
 sites.each {|line|
@@ -15,29 +14,36 @@ sites.each {|line|
 }
 sites.close
 #履歴取得
+count=0
 history.each{|entry|
    hisSet.add(entry.chomp)    
+   count = count + 1
 }
 history.close
-    
+
 #RSS取得を実行する
 rssAry.each {|rss|
    rss.historys=hisSet
    rss.fetchRSS() 
-   rss.postNewEntry()
+   postAry.concat( rss.postNewEntry() )
 }
-#更新履歴を一次ファイルへ書き込む
-rssAry.each{|rss|
-    rss.ary.each{|entry|
-        tmp.puts(entry.url)
+#RSSの履歴を保存
+if count > 500 then
+    #現状の最新RSSで履歴を上書き
+    #puts "rssHistory reset"
+    open("/home/pi/rss/rssHistory","w"){|f|
+        rssAry.each{|rss|
+            rss.ary.each{|entry|
+                f.puts(entry.url)
+            }
+        }
+    }
+end
+#次回のフィルタリングのため今回のエントリーを追記
+open("/home/pi/rss/rssHistory","a"){|f|
+    #puts "Add entry to rssHistory"
+    postAry.each{|url|
+        f.puts(url)
+        #puts "ADD: "+url
     }
 }
-tmp.close
-tmp.open
-#次回のフィルタリングのため今回のエントリーを保存
-open("/home/pi/rss/rssHistory","w"){|f|
-    tmp.each{|line|
-        f.puts(line)
-    }
-}
-tmp.close

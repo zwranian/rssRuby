696acc6a67ae0342818f8105bb4b2e39ccc249fc
zwranian <zawa.pc@gmail.com>
2013-05-17 00:42:38 +0900

    何度もツイートするバグに対応

--- access_token.rb
+++
@@ -1,6 +0,0 @@
-    module AccessToken
-        CONSUMER_KEY        =""
-        CONSUMER_SECRET     =""
-        ACCESS_TOKEN        =""
-        ACCESS_TOKEN_SECRET =""
-    end
--- pocket/accessToken_Pocket.rb
+++
@@ -1,7 +0,0 @@
-module AccessToken_Pocket
-   CONSUMERKEY_POCKET =""
-   ACCESSTOKEN_POCKET =""
-   USERNAME_POCKET    =""
-   PASSWORD_POCKET    =""
-   REDIRECT_URI       ="http://google.com"
-end

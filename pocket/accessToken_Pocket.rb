c7b5bef2bb12384cc7676cbfd11e8e31f2fde926
zwranian <zawa.pc@gmail.com>
2013-05-17 00:50:00 +0900

    削除ミス

---
+++ pocket/accessToken_Pocket.rb
@@ -0,0 +1,7 @@
+module AccessToken_Pocket
+   ACCESSTOKEN_POCKET =""
+   CONSUMERKEY_POCKET =""
+   USERNAME_POCKET    =""
+   PASSWORD_POCKET    =""
+   REDIRECT_URI       ="http://google.com"
+end
